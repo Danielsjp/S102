0a96e550e684ac3042f120ed5843ba767977d3e3
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-23 22:21:04 +0100

    .
    .

---
+++ src/n1exercici3/ArrayIndexOutOfBoundsExample.java
@@ -0,0 +1,15 @@
+package n1exercici3;
+
+public class ArrayIndexOutOfBoundsExample {
+
+  public static int[] exampleVariableOne = new int[10];
+
+  public static void main(String[] args) {
+    try {
+      exampleVariableOne[11] = 9;
+      System.out.println("Array index is valid");
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("ERROR:" + e.getMessage());
+    }
+  }
+}
---
+++ src/n1exercici3/Principal.java
@@ -0,0 +1,12 @@
+package n1exercici3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayIndexOutOfBoundsExample.main(null); 
+		  }
+}
+	
+
+
